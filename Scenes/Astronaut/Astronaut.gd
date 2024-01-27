c9ffdc1737ddf5a86c7c052cd67fe59b993d0cda
Aubrey Taylor <atmatm6000@outlook.com>
2024-01-27 14:21:23 -0600

    armage

--- Scenes/Astronaut/Astronaut.gd
+++ Scenes/Astronaut/Astronaut.gd
@@ -1,26 +1,30 @@
 extends Node2D
 
 @onready var left_arm : Sprite2D = $"Body/Left Arm"
-@onready var left_lower_arm : Sprite2D = $"Body/Left Arm/Left Lower Arm"
+@onready var left_lower_arm : Sprite2D = $"Body/Left Arm/Lower"
 @onready var right_arm : Sprite2D = $"Body/Right Arm"
-#@onready var right_lower_arm : Polygon2D = $"Body/Right Arm/Right Lower Arm"
+@onready var right_lower_arm : Sprite2D = $"Body/Right Arm/Lower"
 
-#@onready var left_leg : Polygon2D = $"Body/Left Leg"
-#@onready var left_calf : Polygon2D = $"Body/Left Leg/Left Calf"
-#@onready var right_leg : Polygon2D = $"Body/Right Leg"
-#@onready var right_calf : Polygon2D = $"Body/Right Leg/Right Calf"
+@onready var left_leg : Sprite2D = $"Body/Left Leg"
+@onready var left_calf : Sprite2D = $"Body/Left Leg/Lower"
+@onready var right_leg : Sprite2D = $"Body/Right Leg"
+@onready var right_calf : Sprite2D = $"Body/Right Leg/Lower"
 
-#@onready var arms : Array[Polygon2D] = [left_arm, right_arm]
-#@onready var lower_arms : Array[Polygon2D] = [left_lower_arm, right_lower_arm]
-#@onready var legs : Array[Polygon2D] = [left_leg, right_leg]
-#@onready var calves : Array[Polygon2D] = [left_calf, right_calf]
-@onready var limbs : Array[Sprite2D] = [left_arm]
-@onready var lower_limbs : Array[Sprite2D] = [left_lower_arm]
+@onready var arms : Array[Sprite2D] = [left_arm, right_arm]
+@onready var lower_arms : Array[Sprite2D] = [left_lower_arm, right_lower_arm]
+@onready var legs : Array[Sprite2D] = [left_leg, right_leg]
+@onready var calves : Array[Sprite2D] = [left_calf, right_calf]
+@onready var limbs : Array[Sprite2D] = [left_arm, right_arm, left_leg, right_leg]
+@onready var lower_limbs : Array[Sprite2D] = [left_lower_arm, right_lower_arm, left_calf, right_calf]
 
+var step = 0.0;
 
 func _process(delta):
+	step += delta * 0.5;
 	for limb : Sprite2D in limbs:
-		limb.transform = limb.transform.rotated_local(delta)
+		limb.transform = limb.transform.rotated_local(sin(step));
 	for lower_limb : Sprite2D in lower_limbs:
-		lower_limb.transform = lower_limb.transform.rotated_local(delta)
+		lower_limb.transform = lower_limb.transform.rotated_local(cos(step));
+		#lower_limb.transform = lower_limb.transform.rotated_local(delta)
+	transform = transform.rotated_local(tan(step))
 	pass
