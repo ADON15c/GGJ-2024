f71b5befa1bd7596dc7aa88c7ecfccf5d211aa6d
Aubrey Taylor <atmatm6000@outlook.com>
2024-01-28 14:06:31 -0600

    Light score fix

--- Scenes/Score/Score.gd
+++ Scenes/Score/Score.gd
@@ -4,12 +4,11 @@ extends Node2D
 
 @export_group("Extras")
 @export var health_nodes: Array[Node2D] = [];
-var score = 0;
 var health = 3;
 const health_bonus = 5;
 
 func _process(delta):
-	$Label.text = "Score: %s" % [score];
+	$Label.text = "Score: %s" % [game_state.score];
 	for i in health_nodes.size():
 		(health_nodes[i] as Health).update_body(health <= i);
 
@@ -19,4 +18,6 @@ func _on_fail():
 		get_tree().change_scene_to_file("res://Scenes/Main/Main.tscn")
 func _on_success():
 	game_state.score += 1;
+	if (game_state.score % health_bonus) == 0:
+		health += 1
 
